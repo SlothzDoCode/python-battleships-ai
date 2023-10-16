3a5c23b8dbe802e55dc9f61a3335c270848e6337
SlothzDoCode <146091658+SlothzDoCode@users.noreply.github.com>
2023-10-16 13:21:03 +0100

    Update base.py

--- base.py
+++ base.py
@@ -1,3 +1,56 @@
 import time
 import random
 
+boardRow1 = ["□","□","□","□","□","□","□","□","□","□"]
+boardRow2 = ["□","□","□","□","□","□","□","□","□","□"]
+boardRow3 = ["□","□","□","□","□","□","□","□","□","□"]
+boardRow4 = ["□","□","□","□","□","□","□","□","□","□"]
+boardRow5 = ["□","□","□","□","□","□","□","□","□","□"]
+boardRow6 = ["□","□","□","□","□","□","□","□","□","□"]
+boardRow7 = ["□","□","□","□","□","□","□","□","□","□"]
+boardRow8 = ["□","□","□","□","□","□","□","□","□","□"]
+boardRow9 = ["□","□","□","□","□","□","□","□","□","□"]
+boardRow10 = ["□","□","□","□","□","□","□","□","□","□"]
+
+print(boardRow1)
+print(boardRow2)
+print(boardRow3)
+print(boardRow4)
+print(boardRow5)
+print(boardRow6)
+print(boardRow7)
+print(boardRow8)
+print(boardRow9)
+print(boardRow10)
+print()
+print()
+print()
+
+start = input()
+
+if start != "":
+    start = input()
+
+carrier = 5
+Battle_ship = 4
+cruiser = 3
+Submarine = 3
+destroyer = 2
+
+carrier_spotH = random.randint(1,10)
+carrier_spotV = random.randint(1,10)
+carrier_rotation = random.randint(1,2)
+
+if carrier_spotH == 1:
+    ()
+            
+print(boardRow1)
+print(boardRow2)
+print(boardRow3)
+print(boardRow4)
+print(boardRow5)
+print(boardRow6)
+print(boardRow7)
+print(boardRow8)
+print(boardRow9)
+print(boardRow10)
